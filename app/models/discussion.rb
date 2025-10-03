e07a3e1e29e10fcbcb47bd9c67bb60b74b805dd1
MoulOut <mmoulinlima@outlook.com>
2025-10-03 15:41:38 -0300

    feat: add category filtering

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -9,9 +9,12 @@ class Discussion < ApplicationRecord
 
   accepts_nested_attributes_for :posts
 
+  broadcasts_to :category, inserts_by: :prepend
+
   after_create_commit -> { broadcast_prepend_to %w[discussions discussion] }
   after_update_commit -> { broadcast_replace_to %w[discussions discussion] }
   after_destroy_commit -> { broadcast_remove_to %w[discussions discussion] }
+
   def to_param
     "#{id}-#{name.downcase.truncate(100, separator: " ")}".parameterize
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   resources :discussions, only: %i[index show new create edit update destroy] do
     resources :posts, only: %i[create show edit update destroy], module: :discussions
+
+    collection do
+      get "category/:id", to: "categories/discussions#index", as: :category
+    end
   end
   root to: "discussions#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
